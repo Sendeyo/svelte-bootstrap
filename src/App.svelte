da0dfedcdc5e67a014c282260a3ac3d404dded28
Sendeyo <maxmuthomi@gmail.com>
2023-08-15 12:05:29 +0300

    First Push

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<main>
+	this is it
+	<h1>This is amaizing </h1>
+	<button class="btn btn-outline-info">Hello from the other side
+	</button>
+
+</main>
+
+<style>
+
+</style>
